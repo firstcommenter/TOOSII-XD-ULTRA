7b64838535f2cea015551d3b22e38daf6bbcea2a
toosiike <toosii042@gmail.com>
2026-03-08 11:23:08 +0300

    Create textmakermenu.js

---
+++ library/menulist/textmakermenu.js
@@ -0,0 +1,23 @@
+const textmakerMenu = `
+┏❒ *ᴛᴇxᴛ ᴍᴀᴋᴇʀ* ❒
+┃➤ .metallic [ᴛᴇxᴛ]
+┃➤ .ice [ᴛᴇxᴛ]
+┃➤ .snow [ᴛᴇxᴛ]
+┃➤ .impressive [ᴛᴇxᴛ]
+┃➤ .matrix [ᴛᴇxᴛ]
+┃➤ .light [ᴛᴇxᴛ]
+┃➤ .neon [ᴛᴇxᴛ]
+┃➤ .devil [ᴛᴇxᴛ]
+┃➤ .purple [ᴛᴇxᴛ]
+┃➤ .thunder [ᴛᴇxᴛ]
+┃➤ .leaves [ᴛᴇxᴛ]
+┃➤ .1917 [ᴛᴇxᴛ]
+┃➤ .arena [ᴛᴇxᴛ]
+┃➤ .hacker [ᴛᴇxᴛ]
+┃➤ .sand [ᴛᴇxᴛ]
+┃➤ .blackpink [ᴛᴇxᴛ]
+┃➤ .glitch [ᴛᴇxᴛ]
+┃➤ .fire [ᴛᴇxᴛ]
+┗❒`
+
+module.exports = textmakerMenu
