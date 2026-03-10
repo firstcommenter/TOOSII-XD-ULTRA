edcdc5e1271be7d9196c096da4ac2a22e0d784b3
toosiike <toosii042@gmail.com>
2026-03-10 13:20:12 +0300

    Update othermenu.js

--- library/menulist/othermenu.js
+++ library/menulist/othermenu.js
@@ -1,85 +1,60 @@
 const otherMenu = `
-┏❒ *ʙᴏᴛ ɪɴꜰᴏ* ❒
-┃➤ .sc
-┃➤ .script
-┃➤ .source
-┃➤ .owner
-┃➤ .botinfo
-┃➤ .infobot
-┃➤ .infoserver
-┗❒
+╔══════════════════════════╗
+║  ℹ️  *BOT INFO*
+╚══════════════════════════╝
+  ├ .sc  ├ .script  ├ .source
+  ├ .owner
+  ├ .botinfo
+  ├ .infobot
+  └ .infoserver
 
-┏❒ *ꜰᴜɴ & ꜱᴏᴄɪᴀʟ* ❒
-┃➤ .compliment @ᴜꜱᴇʀ
-┃➤ .insult @ᴜꜱᴇʀ
-┃➤ .flirt
-┃➤ .shayari
-┃➤ .goodnight
-┃➤ .roseday
-┃➤ .character
-┃➤ .joke
-┃➤ .quote
-┃➤ .motivation
-┃➤ .fact
-┃➤ .wasted @ᴜꜱᴇʀ
-┃➤ .namecard [ɴᴀᴍᴇ]
-┃➤ .tweet [ᴛᴇxᴛ]
-┃➤ .ytcomment [ᴛᴇxᴛ]
-┗❒
+╔══════════════════════════╗
+║  😄  *FUN & SOCIAL*
+╚══════════════════════════╝
+  ├ .compliment  › @user
+  ├ .insult      › @user
+  ├ .flirt    ├ .shayari
+  ├ .goodnight ├ .roseday
+  ├ .character ├ .joke
+  ├ .quote    ├ .motivation
+  ├ .fact
+  ├ .wasted    › @user
+  ├ .namecard  › [name]
+  ├ .tweet     › [text]
+  └ .ytcomment › [text]
 
-┏❒ *ᴀɴɪᴍᴇ* ❒
-┃➤ .anime [ɴᴀᴍᴇ]
-┃➤ .waifu
-┃➤ .neko
-┃➤ .loli
-┃➤ .nom
-┃➤ .poke @ᴜꜱᴇʀ
-┃➤ .cry
-┃➤ .kiss @ᴜꜱᴇʀ
-┃➤ .pat @ᴜꜱᴇʀ
-┃➤ .hug @ᴜꜱᴇʀ
-┃➤ .wink
-┃➤ .facepalm
-┗❒
+╔══════════════════════════╗
+║  🎌  *ANIME*
+╚══════════════════════════╝
+  ├ .anime  › [name]
+  ├ .waifu  ├ .neko
+  ├ .loli   ├ .nom
+  ├ .poke   › @user
+  ├ .cry    ├ .wink
+  ├ .kiss   › @user
+  ├ .pat    › @user
+  ├ .hug    › @user
+  └ .facepalm
 
-┏❒ *ᴛᴇxᴛ ᴍᴀᴋᴇʀ* ❒
-┃➤ .metallic [ᴛᴇxᴛ]
-┃➤ .ice [ᴛᴇxᴛ]
-┃➤ .snow [ᴛᴇxᴛ]
-┃➤ .neon [ᴛᴇxᴛ]
-┃➤ .fire [ᴛᴇxᴛ]
-┃➤ .glitch [ᴛᴇxᴛ]
-┃➤ .thunder [ᴛᴇxᴛ]
-┃➤ .matrix [ᴛᴇxᴛ]
-┃➤ .hacker [ᴛᴇxᴛ]
-┃➤ .devil [ᴛᴇxᴛ]
-┃➤ .purple [ᴛᴇxᴛ]
-┃➤ .blackpink [ᴛᴇxᴛ]
-┃➤ .sand [ᴛᴇxᴛ]
-┃➤ .arena [ᴛᴇxᴛ]
-┃➤ .1917 [ᴛᴇxᴛ]
-┃➤ .light [ᴛᴇxᴛ]
-┃➤ .impressive [ᴛᴇxᴛ]
-┃➤ .leaves [ᴛᴇxᴛ]
-┗❒
+╔══════════════════════════╗
+║  🖼️  *IMAGE EDIT*
+╚══════════════════════════╝
+  ├ .heart     › @user
+  ├ .circle    › reply img
+  ├ .jail      › @user
+  ├ .passed    › @user
+  ├ .triggered › @user
+  ├ .comrade   › @user
+  ├ .lgbt      › @user
+  ├ .lolice    › @user
+  └ .glass     › reply img
 
-┏❒ *ɪᴍᴀɢᴇ ᴇᴅɪᴛ* ❒
-┃➤ .heart @ᴜꜱᴇʀ
-┃➤ .circle ʀᴇᴘʟʏ ɪᴍɢ
-┃➤ .jail @ᴜꜱᴇʀ
-┃➤ .passed @ᴜꜱᴇʀ
-┃➤ .triggered @ᴜꜱᴇʀ
-┃➤ .comrade @ᴜꜱᴇʀ
-┃➤ .lgbt @ᴜꜱᴇʀ
-┃➤ .lolice @ᴜꜱᴇʀ
-┃➤ .glass ʀᴇᴘʟʏ ɪᴍɢ
-┗❒
-
-┏❒ *ɢɪᴛʜᴜʙ* ❒
-┃➤ .github [ᴜꜱᴇʀɴᴀᴍᴇ]
-┃➤ .git [ᴜꜱᴇʀɴᴀᴍᴇ]
-┃➤ .repo [ᴜꜱᴇʀ/ʀᴇᴘᴏ]
-┃➤ .clone [ɢɪᴛʜᴜʙ ᴜʀʟ]
-┗❒`
+╔══════════════════════════╗
+║  🐙  *GITHUB*
+╚══════════════════════════╝
+  ├ .github › [username]
+  ├ .git    › [username]
+  ├ .repo   › [user/repo]
+  └ .clone  › [github url]`
 
 module.exports = otherMenu
