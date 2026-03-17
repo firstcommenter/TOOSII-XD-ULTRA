7576885942c55b8eee38a07375d9ee08fb96314e
toosiike <toosii042@gmail.com>
2026-03-17 19:30:54 +0300

    style: apply ├◈ `.cmd` format to othermenu.js

--- library/menulist/othermenu.js
+++ library/menulist/othermenu.js
@@ -2,63 +2,63 @@ const otherMenu = `
 ╔══════════════════════════╗
 ║  ℹ️  *BOT INFO*
 ╚══════════════════════════╝
-  ├ .sc  ├ .script  ├ .source
-  ├ .owner
-  ├ .botinfo
-  ├ .infobot
-  └ .infoserver
+  ├◈  `.sc`  ├◈  `.script`  ├◈  `.source`
+  ├◈  `.owner`
+  ├◈  `.botinfo`
+  ├◈  `.infobot`
+  └◈  `.infoserver
 
 ╔══════════════════════════╗
 ║  😄  *FUN & SOCIAL*
-╚══════════════════════════╝
-  ├ .advice    ├ .lovemsg
-  ├ .gratitude ├ .thankyou
-  ├ .friendship ├ .pickupline
-  ├ .compliment › @user
-  ├ .insult    › @user
-  ├ .flirt    ├ .shayari
-  ├ .goodnight ├ .roseday
-  ├ .character ├ .joke
-  ├ .quote    ├ .motivation
-  ├ .fact
-  ├ .wasted    › @user
-  ├ .namecard  › [name]
-  ├ .tweet     › [text]
-  └ .ytcomment › [text]
+╚══════════════════════════╝`
+  ├◈  `.advice`    ├◈  `.lovemsg`
+  ├◈  `.gratitude` ├◈  `.thankyou`
+  ├◈  `.friendship` ├◈  `.pickupline`
+  ├◈  `.compliment` › @user
+  ├◈  `.insult`    › @user
+  ├◈  `.flirt`    ├◈  `.shayari`
+  ├◈  `.goodnight` ├◈  `.roseday`
+  ├◈  `.character` ├◈  `.joke`
+  ├◈  `.quote`    ├◈  `.motivation`
+  ├◈  `.fact`
+  ├◈  `.wasted`    › @user
+  ├◈  `.namecard`  › [name]
+  ├◈  `.tweet`     › [text]
+  └◈  `.ytcomment` › [text]
 
 ╔══════════════════════════╗
 ║  🎌  *ANIME*
 ╚══════════════════════════╝
-  ├ .anime      › [name]
-  ├ .animequote ├ .animerandom
-  ├ .waifu    ├ .neko
-  ├ .loli     ├ .nom
-  ├ .poke     › @user
-  ├ .cry      ├ .wink
-  ├ .kiss     › @user
-  ├ .pat      › @user
-  ├ .hug      › @user
-  └ .facepalm
+  ├◈  `.anime`      › [name]
+  ├◈  `.animequote` ├◈  `.animerandom`
+  ├◈  `.waifu`    ├◈  `.neko`
+  ├◈  `.loli`     ├◈  `.nom`
+  ├◈  `.poke`     › @user
+  ├◈  `.cry`      ├◈  `.wink`
+  ├◈  `.kiss`     › @user
+  ├◈  `.pat`      › @user
+  ├◈  `.hug`      › @user
+  └◈  `.facepalm
 
 ╔══════════════════════════╗
 ║  🖼️  *IMAGE EDIT*
-╚══════════════════════════╝
-  ├ .heart     › @user
-  ├ .circle    › reply img
-  ├ .jail      › @user
-  ├ .passed    › @user
-  ├ .triggered › @user
-  ├ .comrade   › @user
-  ├ .lgbt      › @user
-  ├ .lolice    › @user
-  └ .glass     › reply img
+╚══════════════════════════╝`
+  ├◈  `.heart`     › @user
+  ├◈  `.circle`    › reply img
+  ├◈  `.jail`      › @user
+  ├◈  `.passed`    › @user
+  ├◈  `.triggered` › @user
+  ├◈  `.comrade`   › @user
+  ├◈  `.lgbt`      › @user
+  ├◈  `.lolice`    › @user
+  └◈  `.glass`     › reply img
 
 ╔══════════════════════════╗
 ║  🐙  *GITHUB*
 ╚══════════════════════════╝
-  ├ .github › [username]
-  ├ .git    › [username]
-  ├ .repo   › [user/repo]
-  └ .clone  › [github url]`
+  ├◈  `.github` › [username]
+  ├◈  `.git`    › [username]
+  ├◈  `.repo`   › [user/repo]
+  └◈  `.clone`  › [github url]`
 
 module.exports = otherMenu
