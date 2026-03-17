336b1575718a7a2aa34fafc90b354d5533e5d714
toosiike <toosii042@gmail.com>
2026-03-17 19:30:49 +0300

    style: apply ├◈ `.cmd` format to aimenu.js

--- library/menulist/aimenu.js
+++ library/menulist/aimenu.js
@@ -2,38 +2,38 @@ const aiMenu = `
 ╔══════════════════════════╗
 ║  🤖  *AI CHAT*
 ╚══════════════════════════╝
-  ├ .ai              ├ .chatgpt
-  ├ .gpt-4o          ├ .gpt41-mini
-  ├ .gptturbo        ├ .gptlogic
-  ├ .openai          ├ .claudeai
-  ├ .gemini-ai       ├ .gemini-pro
-  ├ .deepseek        ├ .metaai
-  ├ .copilot         ├ .mistral
-  ├ .grok            ├ .perplexity
-  ├ .wormgpt         ├ .birdai
-  ├ .veniceai        ├ .conciseai
-  ├ .llama-ai        ├ .lumin-ai
-  ├ .typli-ai        ├ .poly-ai
-  ├ .quantum-ai      ├ .chatai
-  ├ .chatevery-where ├ .chatbotai
-  ├ .aoyoai          ├ .blackbox-pro
-  ├ .feloai          ├ .aliceai
-  ├ .gemmaai         ├ .velynai
-  ├ .muslimai        ├ .zerogpt
-  ├ .writecream      ├ .yupraai
-  ├ .logic-eai
-  ├ .bible       › [verse/topic]
-  └ .quran       › [surah:ayah/topic]
+  ├◈  `.ai`              ├◈  `.chatgpt`
+  ├◈  `.gpt-4o`          ├◈  `.gpt41-mini`
+  ├◈  `.gptturbo`        ├◈  `.gptlogic`
+  ├◈  `.openai`          ├◈  `.claudeai`
+  ├◈  `.gemini-ai`       ├◈  `.gemini-pro`
+  ├◈  `.deepseek`        ├◈  `.metaai`
+  ├◈  `.copilot`         ├◈  `.mistral`
+  ├◈  `.grok`            ├◈  `.perplexity`
+  ├◈  `.wormgpt`         ├◈  `.birdai`
+  ├◈  `.veniceai`        ├◈  `.conciseai`
+  ├◈  `.llama-ai`        ├◈  `.lumin-ai`
+  ├◈  `.typli-ai`        ├◈  `.poly-ai`
+  ├◈  `.quantum-ai`      ├◈  `.chatai`
+  ├◈  `.chatevery-where` ├◈  `.chatbotai`
+  ├◈  `.aoyoai`          ├◈  `.blackbox-pro`
+  ├◈  `.feloai`          ├◈  `.aliceai`
+  ├◈  `.gemmaai`         ├◈  `.velynai`
+  ├◈  `.muslimai`        ├◈  `.zerogpt`
+  ├◈  `.writecream`      ├◈  `.yupraai`
+  ├◈  `.logic-eai`
+  ├◈  `.bible`       › [verse/topic]
+  └◈  `.quran`       › [surah:ayah/topic]
 
 ╔══════════════════════════╗
 ║  🛠️  *AI TOOLS*
 ╚══════════════════════════╝
-  ├ .vision      › reply image
-  ├ .analyse     › reply image
-  ├ .speechwrite › [topic]
-  ├ .imagine     › [prompt]
-  ├ .flux        › [prompt]
-  ├ .magicstudio › [prompt]
-  └ .totext      › reply image`
+  ├◈  `.vision`      › reply image
+  ├◈  `.analyse`     › reply image
+  ├◈  `.speechwrite` › [topic]
+  ├◈  `.imagine`     › [prompt]
+  ├◈  `.flux`        › [prompt]
+  ├◈  `.magicstudio` › [prompt]
+  └◈  `.totext`      › reply image`
 
 module.exports = aiMenu
