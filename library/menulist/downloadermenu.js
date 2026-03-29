f5b63b622191aa3af98da9b2e61ab6ed04c71632
toosiike <toosii042@gmail.com>
2026-03-29 08:52:53 +0300

    style: remove backticks from command names in downloadermenu

--- library/menulist/downloadermenu.js
+++ library/menulist/downloadermenu.js
@@ -1,18 +1,18 @@
 const downloaderMenu = `
 ╭─⊷ *📥 MEDIA DOWNLOADERS*
 │
-│  • \`.play\`        →  [song query]
-│  • \`.song\`        →  [song query]
-│  • \`.video\`       →  [query / url]
-│  • \`.ytdocplay\`   →  [query]
-│  • \`.ytdocvideo\`  →  [query]
-│  • \`.ig\`          →  [instagram url]
-│  • \`.tt\`          →  [tiktok url]
-│  • \`.fb\`          →  [facebook url]
-│  • \`.spotify\`     →  [song name]
-│  • \`.mediafire\`   →  [mediafire url]
-│  • \`.apk\`         →  [app name]
-│  • \`.gitclone\`    →  [github url]
+│  • .play        →  [song query]
+│  • .song        →  [song query]
+│  • .video       →  [query / url]
+│  • .ytdocplay   →  [query]
+│  • .ytdocvideo  →  [query]
+│  • .ig          →  [instagram url]
+│  • .tt          →  [tiktok url]
+│  • .fb          →  [facebook url]
+│  • .spotify     →  [song name]
+│  • .mediafire   →  [mediafire url]
+│  • .apk         →  [app name]
+│  • .gitclone    →  [github url]
 │
 ╰─⊷
 `
