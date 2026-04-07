9d9bccdeddd2b4de11fe137da3a169f86341dbae
toosiike <toosii042@gmail.com>
2026-04-07 13:55:26 +0300

    feat: add antiGroupMentionGroups to SAVEABLE settings list

--- library/settings.js
+++ library/settings.js
@@ -9,7 +9,8 @@ const SAVEABLE = [
     'packname','authorname','watermark','botPrefix','statusMentionWarns','statusMentionDeleteList',
     'BOT_MODE','onlyGroup','botPublicMode','ownerFontMode','antilinkGcGroups',
     'antiLinkAction','antiLinkWarnLimit','antiLinkWarns',
-    'antiStatusMentionGroups'
+    'antiStatusMentionGroups',
+    'antiGroupMentionGroups'
 ]
 const loadSettings = () => {
     try {
